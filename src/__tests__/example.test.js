50d6a03d87235d67d6e7d8e950508b81fdb5d2c0
JadTermsani <jadtermsani@gmail.com>
2019-02-07 16:18:18 +0200

    chore: add eslint

---
+++ src/__tests__/example.test.js
@@ -0,0 +1,5 @@
+import { add } from '../index';
+
+test('basic test', () => {
+  expect(add(2, 4)).toBe(6);
+});
---
+++ src/example.js
@@ -0,0 +1,5 @@
+export default function add(x, y) {
+  return x + y;
+}
+
+console.log(add(9, 6));
